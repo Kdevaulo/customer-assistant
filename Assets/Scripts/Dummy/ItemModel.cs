d758fd59508092be1298e43919d9b265d702b8b1
Egor Shishigin <36220657+egorshishigin@users.noreply.github.com>
2022-04-14 14:27:52 +0500

    MVP-like item switching/info

---
+++ Assets/Scripts/Dummy/ItemModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dummy
+{
+    public class ItemModel : MonoBehaviour
+    {
+        [SerializeField] private Item[] _items;
+        private int _itemIndex;
+        public event Action ItemChanged;
+        public event Action<bool> InfoClick;
+        public Sprite ItemImage { get; private set; }
+
+        public void NextItem()
+        {
+            if (_itemIndex < _items.Length - 1)
+            {
+                _itemIndex += 1;
+                ItemImage = _items[_itemIndex].ItemImage;
+                ItemChanged?.Invoke();
+            }
+        }
+
+        public void PreviousItem()
+        {
+            _itemIndex -= 1;
+            if (_itemIndex > -1)
+            {
+                ItemImage = _items[_itemIndex].ItemImage;
+            }
+            else if (_itemIndex <= -1)
+            {
+                SetEmptyItem();
+            }
+            ItemChanged?.Invoke();
+        }
+
+        public string GetDescriptionText()
+        {
+            if (_itemIndex == -1)
+                return null;
+            string descriptionText = $"{_items[_itemIndex].ItemName}\n{_items[_itemIndex].ShopName}\n{_items[_itemIndex].ItemPrice}";
+            return descriptionText;
+        }
+
+        public void ShowInfo(bool clicked)
+        {
+            if (ItemImage != null)
+            {
+                InfoClick?.Invoke(clicked);
+            }
+        }
+
+        private void SetEmptyItem()
+        {
+            _itemIndex = -1;
+            ItemImage = null;
+        }
+
+        public void Init()
+        {
+            _itemIndex = 0;
+            ItemImage = _items[_itemIndex].ItemImage;
+        }
+    }
+}
---
+++ Assets/Scripts/Dummy/ItemPresenter.cs
@@ -0,0 +1,66 @@
+﻿namespace Dummy
+{
+    public class ItemPresenter
+    {
+        private ItemView _view;
+        private ItemModel _model;
+
+        public ItemPresenter(ItemView view, ItemModel model)
+        {
+            _view = view;
+            _model = model;
+        }
+
+        public void Enable()
+        {
+            _model.ItemChanged += OnItemChange;
+            _model.InfoClick += OnInfoClick;
+            _view.NextClick += OnViewNextClick;
+            _view.PreviousClick += OnViewPreviousClick;
+            _view.InfoClick += OnViewClick;
+        }
+
+        public void Disable()
+        {
+            _model.ItemChanged -= OnItemChange;
+            _model.InfoClick -= OnInfoClick;
+            _view.NextClick -= OnViewNextClick;
+            _view.PreviousClick -= OnViewPreviousClick;
+            _view.InfoClick -= OnViewClick;
+        }
+
+        private void OnItemChange()
+        {
+            _view.SetIcon(_model.ItemImage);
+            _view.DescriptionPanel.SetActive(false);
+            _view.InfoToggle.isOn = false;
+        }
+
+        private void OnViewNextClick()
+        {
+            _model.NextItem();
+            OnItemChange();
+        }
+
+        private void OnViewPreviousClick()
+        {
+            _model.PreviousItem();
+            OnItemChange();
+        }
+
+        private void OnInfoClick(bool clicked)
+        {
+            string descriptionText = _model.GetDescriptionText();
+            if (descriptionText == null)
+                return;
+            _view.SetDescriptionText(descriptionText);
+            _view.DescriptionPanel.SetActive(clicked);
+        }
+
+        private void OnViewClick(bool clicked)
+        {
+            _model.ShowInfo(clicked);
+            OnInfoClick(clicked);
+        }
+    }
+}
