42acee46344574c5803ec708bd00e080175f4d92
Egor Shishigin <36220657+egorshishigin@users.noreply.github.com>
2022-04-14 14:28:15 +0500

    Simple item switching

---
+++ Assets/Scripts/Dummy/ItemSwitcher.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Dummy
+{
+    public class ItemSwitcher : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _itemSprite;
+        [SerializeField] private Item[] _items;
+        private int _itemsCount = 0;
+        private const int EmptyItemIndex = -1;
+
+        private void Start()
+        {
+            SetEmptyItem();
+        }
+
+        public void NextItem()
+        {
+            _itemsCount++;
+            if (_itemsCount > _items.Length - 1)
+            {
+                _itemsCount = 0;
+            }
+            _itemSprite.sprite = _items[_itemsCount].ItemImage;
+        }
+
+        public void PrevItem()
+        {
+            _itemsCount--;
+            if (_itemsCount <= EmptyItemIndex)
+            {
+                SetEmptyItem();
+            }
+            else
+            {
+                _itemSprite.sprite = _items[_itemsCount].ItemImage;
+            }
+        }
+
+        private void SetEmptyItem()
+        {
+            _itemSprite.sprite = null;
+            _itemsCount = EmptyItemIndex;
+        }
+    }
+}
