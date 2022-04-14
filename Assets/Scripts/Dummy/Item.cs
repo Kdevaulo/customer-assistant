2594421fc6b40cf7f64fb1a7a3cf6749627d36bc
Egor Shishigin <36220657+egorshishigin@users.noreply.github.com>
2022-04-14 14:23:09 +0500

    Item SO class

---
+++ Assets/Scripts/Dummy/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Dummy
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "Item")]
+    public class Item : ScriptableObject
+    {
+        public string ItemName => _itemName;
+        public Sprite ItemImage => _itemIMage;
+        public float ItemPrice => _price;
+        public string ShopName => _shopName;
+
+        [SerializeField] private string _itemName;
+        [SerializeField] private Sprite _itemIMage;
+        [SerializeField] private float _price;
+        [SerializeField] private string _shopName;
+    }
+}
