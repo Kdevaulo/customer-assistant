8cbab0e2d9be3815d1efa5c23ea26d39e305de46
Egor Shishigin <36220657+egorshishigin@users.noreply.github.com>
2022-05-04 15:38:47 +0500

    Shop model added

---
+++ Assets/Scripts/DummyActivity/Models/ShopModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace DummyActivity.Models
+{
+    [Serializable]
+    public class ShopModel
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private string _address;
+
+        public string Name => _name;
+        public string Address => _address;
+    }
+}
