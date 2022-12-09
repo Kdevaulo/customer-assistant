7c1c9f0ea7c92268fd1588506714029961cb1eb1
Nikita Mikhailov <kdevaulo@gmail.com>
2022-12-09 18:42:28 +0500

    Remove button logic implemented

--- Assets/Scripts/FavoritesActivity/Controllers/ActivityController.cs
+++ Assets/Scripts/FavoritesActivity/Controllers/ActivityController.cs
@@ -11,6 +11,8 @@ using Mapbox.Json;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using Object = UnityEngine.Object;
+
 namespace CustomerAssistant.FavoritesActivity.Controllers
 {
     public class ActivityController : IDisposable
@@ -21,7 +23,14 @@ namespace CustomerAssistant.FavoritesActivity.Controllers
 
         private readonly FavoriteImageView _favoriteContainerPrefab;
 
-        private List<Product> _favoriteProducts = new List<Product>();
+        private readonly List<Product> _favoriteProducts = new List<Product>();
+
+        private readonly List<FavoriteImageView> _favoriteViews = new List<FavoriteImageView>();
+
+        private Dictionary<FavoriteImageView, Product> _productViewCollection =
+            new Dictionary<FavoriteImageView, Product>();
+
+        private Dictionary<Product, string> _productKeyCollection = new Dictionary<Product, string>();
 
         public ActivityController(BackButtonView backBackButtonView,
             RectTransform contentContainer, FavoriteImageView favoriteContainerPrefab)
@@ -44,6 +53,7 @@ namespace CustomerAssistant.FavoritesActivity.Controllers
 
             LoadFavorites();
             InitializeFavorites();
+            UpdateFavoritesPositions();
         }
 
         private void LoadFavorites()
@@ -52,6 +62,7 @@ namespace CustomerAssistant.FavoritesActivity.Controllers
 
             for (int i = 0; i < Constants.MaxFavorites; i++)
             {
+                // todo: remove key dependency on index using route table
                 var key = string.Format(Constants.PrefsKeyPattern, i);
 
                 if (!PlayerPrefs.HasKey(key))
@@ -59,44 +70,104 @@ namespace CustomerAssistant.FavoritesActivity.Controllers
                     break;
                 }
 
-                Debug.Log($"got prefs at \"{key}\"");
-
                 var serializedString = PlayerPrefs.GetString(key);
 
                 var product = JsonConvert.DeserializeObject<Product>(serializedString);
 
                 product.Sprite = Utils.ConvertSprite(product.Image);
 
+                _productKeyCollection.Add(product, key);
                 _favoriteProducts.Add(product);
             }
         }
 
         private void InitializeFavorites()
         {
-            var targetHeight = Math.Abs(_favoriteProducts.Count * Constants.FavoritesYOffset);
+            ChangeContainerHeight(_favoriteProducts.Count);
 
-            var contentContainerRect = _contentContainer.rect;
+            _productViewCollection.Clear();
+            _favoriteViews.Clear();
 
-            if (targetHeight > contentContainerRect.height)
+            foreach (var favoriteProduct in _favoriteProducts)
             {
-                _contentContainer.sizeDelta = new Vector2(_contentContainer.sizeDelta.x, targetHeight);
-            }
+                var spawnedView = Object.Instantiate(_favoriteContainerPrefab, _contentContainer);
 
-            for (var i = 0; i < _favoriteProducts.Count; i++)
-            {
-                var spawnedView = UnityEngine.Object.Instantiate(_favoriteContainerPrefab, _contentContainer);
+                SubscribeButtons(spawnedView);
 
                 var targetImage = spawnedView.TargetImage;
-                targetImage.sprite = _favoriteProducts[i].Sprite;
+                targetImage.sprite = favoriteProduct.Sprite;
                 targetImage.SetNativeSize();
 
                 var ratio = targetImage.sprite.rect.size.y / spawnedView.ObjectContainerRectTransform.rect.height;
 
                 targetImage.rectTransform.localScale = new Vector3(1 / ratio, 1 / ratio, 1);
 
-                spawnedView.FavoriteRectTransform.anchoredPosition =
-                    new Vector2(spawnedView.ImageRectTransform.position.x, Constants.FavoritesYOffset * i);
+                _favoriteViews.Add(spawnedView);
+                _productViewCollection.Add(spawnedView, favoriteProduct);
             }
+        }
+
+        private void HandleRemoveButtonClick(FavoriteImageView imageView)
+        {
+            UnsubscribeButtons(imageView);
+
+            var productForRemoving = _productViewCollection[imageView];
+            _favoriteProducts.Remove(productForRemoving);
+            _favoriteViews.Remove(imageView);
+
+            TryRemoveFromPrefs(_productKeyCollection[productForRemoving]);
+
+            var removableGameObject = imageView.gameObject;
+            removableGameObject.SetActive(false);
+            Object.Destroy(removableGameObject);
+
+            ChangeContainerHeight(_favoriteProducts.Count);
+            UpdateFavoritesPositions();
+        }
+
+        private void HandleUseButtonClick(FavoriteImageView imageView)
+        {
+            // todo: add to playerPrefs
+        }
+
+        private void ChangeContainerHeight(int productsCount)
+        {
+            var targetHeight = Math.Abs(productsCount * Constants.FavoritesYOffset);
+
+            if (targetHeight > _contentContainer.rect.height)
+            {
+                _contentContainer.sizeDelta = new Vector2(_contentContainer.sizeDelta.x, targetHeight);
+            }
+        }
+
+        private void UpdateFavoritesPositions()
+        {
+            for (var i = 0; i < _favoriteViews.Count; i++)
+            {
+                var view = _favoriteViews[i];
+                view.FavoriteRectTransform.anchoredPosition = new Vector2(view.ImageRectTransform.position.x,
+                    Constants.FavoritesYOffset * i);
+            }
+        }
+
+        private void TryRemoveFromPrefs(string key)
+        {
+            if (PlayerPrefs.HasKey(key))
+            {
+                PlayerPrefs.DeleteKey(key);
+            }
+        }
+
+        private void SubscribeButtons(FavoriteImageView imageView)
+        {
+            imageView.RemoveButtonClicked += HandleRemoveButtonClick;
+            imageView.UseButtonClicked += HandleUseButtonClick;
+        }
+
+        private void UnsubscribeButtons(FavoriteImageView imageView)
+        {
+            imageView.RemoveButtonClicked -= HandleRemoveButtonClick;
+            imageView.UseButtonClicked -= HandleUseButtonClick;
         }
 
         private void HandleBackBackButtonClick()
--- Assets/Scripts/FavoritesActivity/Views/FavoriteImageView.cs
+++ Assets/Scripts/FavoritesActivity/Views/FavoriteImageView.cs
@@ -1,3 +1,5 @@
+using System;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,16 +7,14 @@ namespace CustomerAssistant.FavoritesActivity.Views
 {
     public class FavoriteImageView : MonoBehaviour
     {
+        public event Action<FavoriteImageView> RemoveButtonClicked = delegate { };
+        public event Action<FavoriteImageView> UseButtonClicked = delegate { };
+
         public Image TargetImage => _targetImage;
 
         public RectTransform FavoriteRectTransform => _favoriteRectTransform;
         public RectTransform ImageRectTransform => _imageRectTransform;
-
         public RectTransform ObjectContainerRectTransform => _objectContainerRectTransform;
-
-        public Button RemoveButton => _removeButton;
-
-        public Button UseButton => _useButton;
 
         [SerializeField] private Image _targetImage;
 
@@ -27,5 +27,27 @@ namespace CustomerAssistant.FavoritesActivity.Views
         [SerializeField] private Button _removeButton;
 
         [SerializeField] private Button _useButton;
+
+        private void OnEnable()
+        {
+            _removeButton.onClick.AddListener(HandleRemoveButtonClick);
+            _useButton.onClick.AddListener(HandleUseButtonClick);
+        }
+
+        private void OnDisable()
+        {
+            _removeButton.onClick.RemoveListener(HandleRemoveButtonClick);
+            _useButton.onClick.RemoveListener(HandleUseButtonClick);
+        }
+
+        private void HandleRemoveButtonClick()
+        {
+            RemoveButtonClicked.Invoke(this);
+        }
+
+        private void HandleUseButtonClick()
+        {
+            UseButtonClicked.Invoke(this);
+        }
     }
 }
